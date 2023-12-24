41348ed40229a7359d068465e2ac6b03788e8d02
betweenTheBrackets <betweenthebrackets.info@gmail.com>
2023-12-24 10:38:17 -0500

    day 9 complete

---
+++ day-9/part2.js
@@ -0,0 +1,45 @@
+const fs = require('node:fs')
+
+main()
+
+function main() {
+    // const input = fs.readFileSync(__dirname + '/sample_input.txt', 'utf8')
+    const input = fs.readFileSync(__dirname + '/input.txt', 'utf8')
+
+    const histories = input.split('\n').map((_) => _.split(' ').map((_) => parseInt(_)))
+    console.log(histories)
+
+    const predictions = []
+
+    for (let i = 0; i < histories.length; i++) {
+        let curHistory = histories[i]
+        const stepsFunnel = []
+        let steps = []
+
+        do {
+            for (let j = 0; j < curHistory.length - 1; j++) {
+                const diff = curHistory[j + 1] - curHistory[j]
+                steps.push(diff)
+            }
+
+            curHistory = steps
+            stepsFunnel.push(steps)
+            steps = []
+        } while (!curHistory.every((_) => _ === 0))
+
+        console.log(stepsFunnel)
+
+        let result = 0
+
+        for (let j = stepsFunnel.length - 1; j >= 0; j--) {
+            result = stepsFunnel[j][0] - result
+        }
+
+        result = histories[i][0] - result
+
+        predictions.push(result)
+    }
+
+    console.log(predictions)
+    console.log(predictions.reduce((acc, cur) => acc + cur))
+}
